13c16abef74221aa244bd9768e1dfcb69f2f6930
mehedy201 <mehedy643@gmail.com>
2024-02-29 01:10:20 +0600

    complete front end youtube oac component inside user profile page

--- src/UserAdminDashboard/UserProfile/UserProfileComponents/UserYoutubeOacRequestComponent.jsx
+++ src/UserAdminDashboard/UserProfile/UserProfileComponents/UserYoutubeOacRequestComponent.jsx
@@ -1,7 +1,15 @@
 import { ChevronLeftIcon } from "@heroicons/react/24/solid";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const UserYoutubeOacRequestComponent = () => {
+
+    const { register, handleSubmit, formState: { errors }} = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
+
     return (
         <div>
             <div className="my-4">
@@ -9,13 +17,28 @@ const UserYoutubeOacRequestComponent = () => {
             </div>
             <div>
                 <h2 className="text-lg font-semibold text-slate-500 px-2">Youtube OAC Request</h2>
-                <div className="p-3 border mt-2 rounded-lg">
-                    <p className="my-1 text-sm font-semibold text-slate-500 ms-2">Demo</p>
-                    <input type="text" placeholder="Demo" className="input rounded-full input-bordered w-full"/>
-                    {/* <input type="text" placeholder="Last Name" className="input rounded-full input-bordered w-full" {...register("nick_name", { required: true})}/> */}
-                    {/* {errors.nick_name && <span className='text-red-600 pt-2 block'>Nick/Display Name Required</span>} */}
+                <form onSubmit={handleSubmit(onSubmit)} className="p-3 border mt-2 rounded-lg">
+                    <p className="my-1 text-sm font-semibold text-slate-500 ms-2">Youtube Chanel Link <span className="text-red-500">*</span></p>
+                    <input type="text" placeholder="Youtube Chanel Link" className="input rounded-full input-bordered w-full" {...register("youtubeChanelLink", { required: true})}/>
+                    {errors.youtubeChanelLink && <span className='text-red-600 pt-2 block'>Youtube Chanel Link Required</span>}
+
+                    <p className="mt-3 text-sm font-semibold text-slate-500 ms-2">YouTube Topic Channel URL Required <span className="text-red-500">*</span></p>
+                    <input type="text" placeholder="YouTube Topic Channel URL" className="input rounded-full input-bordered w-full" {...register("youTubeTopicChannelURL", { required: true})}/>
+                    <div className="mt-1">
+                        <span className="text-xs bg-slate-100 text-slate-500 font-bold mx-2 px-2 py-1 rounded-md">Channel that is created for YouTube Music. Example: Artist Name - Topic</span>
+                    </div>
+                    {errors.youtubeChanelLink && <span className='text-red-600 pt-2 block'>YouTube Topic Channel URL Required</span>}
+
+                    <p className="mt-3 text-sm font-semibold text-slate-500 ms-2">Vevo Channel URL (if you have one)</p>
+                    <input type="text" placeholder="Vevo Channel URL" className="input rounded-full input-bordered w-full" {...register("vevoChanelUrl")}/>
+
+                    <p className="mt-3 text-sm font-semibold text-slate-500 ms-2">URL of Official Video or Art Track we distributed <span className="text-red-500">*</span></p>
+                    <input type="text" placeholder="URL of Official Video or Art Track we distributed" className="input rounded-full input-bordered w-full" {...register("distributedUrl", { required: true})}/>
+                    {errors.distributedUrl && <span className='text-red-600 pt-2 block'>YouTube Topic Channel URL Required</span>}
+
+
                     <input type="submit" value={'Update'} className="btn btn-sm my-4 px-6 btn-accent rounded-full" />
-                </div>
+                </form>
             </div>
         </div>
     );
