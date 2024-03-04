dbae57ffd9dba840b49643c68990823cabef9538
mehedy201 <mehedy643@gmail.com>
2024-03-05 02:19:47 +0600

    now set loading when fetch artist data in artist page then show loading

--- src/UserAdminDashboard/UserArtistPage/UserArtistPage.jsx
+++ src/UserAdminDashboard/UserArtistPage/UserArtistPage.jsx
@@ -14,14 +14,17 @@ const UserArtistPage = () => {
 
     const [artistData, setArtistData] = useState();
     const [searchArtistData, setSearchArtistData] = useState();
-    // const [refatchData, setRefatchData] = useState(1)
+    const [fetchLoading, setFetchLoading] = useState(false)
     useEffect( () => {
+      setFetchLoading(true)
       axios.get(`http://localhost:5000/api/v1/artist/${userNameIdRoll[1]}`)
           .then( res => {
+              setFetchLoading(false);
               setSearchArtistData(res.data.data)
               setArtistData(res.data.data)
           })
           .catch(er => console.log(er));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     },[refatchArtistData])
 
     const handleSearch = (e) => {
@@ -34,44 +37,46 @@ const UserArtistPage = () => {
 
     return (
         <div className="md:flex md:h-full">
-            <div className='h-full md:basis-3/4 overflow-y-auto md:border-r p-2'>
-                <div className="mb-4">
-                    <button><Link className="px-2 py-1 font-semibold text-sm text-slate-500 flex items-center inline bg-slate-200 rounded-md" to={'/'}><ChevronLeftIcon className="w-4 h-4 me-1 font-bold"/>Back</Link></button>
-                </div>
-                {/* Search and Create Artist Section ______________________________________________________________________________ */}
-                <div className="md:flex md:justify-between md:items-center bg-slate-50 py-2 px-2 rounded-lg">
-                    <div className="my-2">
-                        <input type="text" onChange={e => handleSearch(e.target.value)} placeholder="Type to Search" className="input input-sm rounded-full input-bordered w-full"/>
-                    </div>
-                    <div className="my-2">
-                        <button onClick={()=>document.getElementById('create_artist_modal').showModal()} className='btn btn-neutral py-1 px-6 rounded-full btn-sm border-none me-2 w-full'>Create Artist</button>
-                    </div>
-                </div>
-                    {/* Create Artist form with Modal Start _______________________________________________________________________ */}
-                    <dialog id="create_artist_modal" className="modal"> 
-                        <div className="modal-box">
-                          <CreateArtistForm/>
-                        </div>
-                    </dialog>
-                    {/* Create Artist form with Modal End _______________________________________________________________________ */}
+            <div className='h-full md:basis-3/4 overflow-y-auto md:border-r'>
+                <div className="sticky top-0 z-10 bg-white p-2">
+                  <div className="mb-4">
+                      <button><Link className="px-2 py-1 font-semibold text-sm text-slate-500 flex items-center inline bg-slate-200 rounded-md" to={'/'}><ChevronLeftIcon className="w-4 h-4 me-1 font-bold"/>Back</Link></button>
+                  </div>
+                  {/* Search and Create Artist Section ______________________________________________________________________________ */}
+                  <div className="md:flex md:justify-between md:items-center bg-slate-50 py-2 px-2 rounded-lg">
+                      <div className="my-2">
+                          <input type="text" onChange={e => handleSearch(e.target.value)} placeholder="Type to Search" className="input input-sm rounded-full input-bordered w-full"/>
+                      </div>
+                      <div className="my-2">
+                          <button onClick={()=>document.getElementById('create_artist_modal').showModal()} className='btn btn-neutral py-1 px-6 rounded-full btn-sm border-none me-2 w-full'>Create Artist</button>
+                      </div>
+                  </div>
+                      {/* Create Artist form with Modal Start _______________________________________________________________________ */}
+                      <dialog id="create_artist_modal" className="modal"> 
+                          <div className="modal-box">
+                            <CreateArtistForm/>
+                          </div>
+                      </dialog>
+                      {/* Create Artist form with Modal End _______________________________________________________________________ */}
 
-                {/* Total Artist Count Section _____________________________________________________________________________________ */}
-                <div className="flex justify-between items-center my-3">
-                    <div className="flex items-center">
-                        <ExclamationCircleIcon className="w-6 h-6 me-1 text-slate-500"/>
-                        Artist Count
-                    </div>
-                    <div><span className="text-sm font-bold">10</span> <span className="ms-1 p-2 bg-slate-50 rounded-md text-sm font-bold">50</span> </div>
-                </div>
+                  {/* Total Artist Count Section _____________________________________________________________________________________ */}
+                  <div className="flex justify-between items-center my-3">
+                      <div className="flex items-center">
+                          <ExclamationCircleIcon className="w-6 h-6 me-1 text-slate-500"/>
+                          Artist Count
+                      </div>
+                      <div><span className="text-sm font-bold">10</span> <span className="ms-1 p-2 bg-slate-50 rounded-md text-sm font-bold">50</span> </div>
+                  </div>
 
-                {/* Artist List and Relase Title Section _____________________________________________________________________________ */}
-                <div className="flex justify-between items-center py-2 rounded-full bg-slate-100 px-4">
-                    <h4 className="font-bold text-slate-600">Profile</h4>
-                    <h4 className="font-bold text-slate-600">Releases</h4>
+                  {/* Artist List and Relase Title Section _____________________________________________________________________________ */}
+                  <div className="flex justify-between items-center py-2 rounded-full bg-slate-100 px-4">
+                      <h4 className="font-bold text-slate-600">Profile</h4>
+                      <h4 className="font-bold text-slate-600">Releases</h4>
+                  </div>
                 </div>
                 
                 {/* Show All Artist Data __________________________________________________________________________________________________ */}
-                <main className="my-4">
+                <main className="my-2 p-2">
                   {
                     artistData?.map((data) => 
                       <div key={data._id} className="flex items-center justify-between p-1 my-1 rounded-md">
@@ -95,7 +100,10 @@ const UserArtistPage = () => {
                     )
                   }
                   {
-                    !artistData &&  <Empty className="pt-12" />
+                    fetchLoading == true && <div className="mt-4 flex items-center justify-center"><span className="loading loading-spinner loading-md me-2"></span></div>
+                  }
+                  {
+                    !artistData && !fetchLoading && <Empty className="pt-12" />
                   }
                   
                 </main>
