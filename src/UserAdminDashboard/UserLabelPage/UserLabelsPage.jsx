33cb454a71051f8bf5440ed960b9a9ca8fc808ae
mehedy201 <mehedy643@gmail.com>
2024-03-07 10:34:20 +0600

    completely done create labels form

--- src/UserAdminDashboard/UserLabelPage/UserLabelsPage.jsx
+++ src/UserAdminDashboard/UserLabelPage/UserLabelsPage.jsx
@@ -1,48 +1,12 @@
 import { BellIcon, ChevronLeftIcon, ExclamationCircleIcon } from "@heroicons/react/24/solid";
-import axios from "axios";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import CreateLabelsForm from "./CreateLabelsForm";
 
 const UserLabelsPage = () => {
 
-    const [errorMessage, setErrorMessage] = useState('');
-    const [upLoadLoading, setUploadLoading] = useState(false);
-    const [uploadedImage, setUploadedImage] = useState('');
-    const [uploadedImageLink, setUploadedImageLink] = useState('')
-
-    const labelsImageUpload = (e) => {
-      setUploadLoading(true)
-      const file = e[0];
-      const formData = new FormData();
-      formData.append('file', file);
-
-      // Check image size ___________________________________
-      if (file.size > 1000000) {
-      setErrorMessage('Image size must be less than 1 MB.');
-      return;
-      }
-
-      axios.post('http://localhost:5000/api/v1/users/upload-labels-img', formData)
-          .then(res => {
-              if(res.status == 200){
-                setUploadedImageLink(res.data.data.imgUrl);
-                setUploadedImage(res.data.data)
-                setUploadLoading(false)
-              }
-          })
-          .catch(er => console.log(er))
-    }
+    
 
   
-
-
-
-    // React Hook Form For Create New Artist _________________________
-    const { register, handleSubmit, formState: { errors }} = useForm();
-    const onSubmit = async (data) => {
-        console.log(data, uploadedImage);
-    }
 
 
 
@@ -65,48 +29,10 @@ const UserLabelsPage = () => {
                     {/* Create Artist form with Modal Start _______________________________________________________________________ */}
                     <dialog id="create_artist_modal" className="modal"> 
                         <div className="modal-box">
-                            {/* Modal Close Right top Side Icon ___________________________________________________ */}
-                            <form method="dialog">
-                                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-                            </form>
-                            {/* Modal Body and Form for create Artist______________________________________________ */}
-                            <div>
-                                <h4 className="font-bold text-slate-500">Create New Label</h4>
-                                <div className="border rounded-lg p-2">
-
-                                <p className="my-1 text-sm font-semibold text-slate-500">Upload Label Image</p>
-                                <img className="h-24 w-24 my-2 rounded-md bg-slate-300" src={uploadedImageLink} alt="" />
-                                <div className="flex items-center ">
-                                    {
-                                        upLoadLoading && <span className="block loading loading-spinner loading-md me-2"></span>
-                                    }
-                                    <input type="file" accept="image/*" id="fileInput" name='image' onChange={e => labelsImageUpload(e.target.files)} />
-                                </div>
-                                {errorMessage && <p className="font-bold text-red-500">{errorMessage}</p>}
-
-                            </div>
-                                {/* _________________________________ */}
-                                <form onSubmit={handleSubmit(onSubmit)}>
-                                    <p className="mt-3 text-sm font-semibold text-slate-500 ms-2">Label Name <span className="text-red-500">*</span></p> 
-                                    <input type="text" className="input rounded-full input-bordered w-full" {...register("labelName", { required: true})}/>
-                                    {errors.artistName && <span className='text-red-600 pt-2 block'>Label Name Required</span>}
-
-                                    <div className="flex items-center py-3">
-                                      {
-                                          upLoadLoading && <span className="block loading loading-spinner loading-md me-2"></span>
-                                      }
-                                      <input type="submit" className="btn btn-sm rounded-full btn-neutral" value="Create" />
-                                    </div>
-                                </form>
-                            </div>
+                            <CreateLabelsForm/>
                         </div>
                     </dialog>
                     {/* Create Artist form with Modal End _______________________________________________________________________ */}
-
-
-
-
-
 
 
                 {/* Total Artist Count Section _____________________________________________________________________________________ */}
