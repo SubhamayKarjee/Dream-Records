b0dc116505190a56c3f8fc002604925a469dd076
mehedy201 <mehedy643@gmail.com>
2024-05-02 13:02:22 +0600

    remove sideber in user profile page

--- src/UserAdminDashboard/UserProfile/UserProfile.jsx
+++ src/UserAdminDashboard/UserProfile/UserProfile.jsx
@@ -1,4 +1,3 @@
-import { BellIcon } from "@heroicons/react/24/solid";
 import { Image } from "antd";
 import { useContext } from "react";
 import { Outlet } from "react-router-dom";
@@ -11,8 +10,8 @@ const UserProfile = () => {
 
 
     return (
-        <div className="md:flex md:h-full">
-            <div className='h-full md:basis-3/4 overflow-y-auto md:border-r p-2'>
+        <div className="md:h-full">
+            <div className='h-full overflow-y-auto md:border-r p-2'>
                 <div className='flex p-2 rounded-md bg-[#EF4136]'>
                     <div className="avatar me-2">
                         <div className="w-16 rounded-md">
@@ -46,13 +45,6 @@ const UserProfile = () => {
                 </div>
                 <div>
                     <Outlet/>
-                </div>
-            </div>
-
-            {/* Blog Post Div  _______________________________*/}
-            <div className="md:basis-1/4">
-                <div className='p-2 border-b'>
-                    <h4 className='flex items-center font-bold text-lg text-slate-500'> <BellIcon className='w-6 h-6 me-2 text-slate-500'/> Notification</h4>
                 </div>
             </div>
         </div>
