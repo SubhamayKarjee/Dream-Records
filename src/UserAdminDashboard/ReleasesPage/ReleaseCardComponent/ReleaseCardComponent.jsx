6c67c6656c0a1ec007b4742084c3aa1c3447ef86
mehedy201 <mehedy643@gmail.com>
2024-09-24 12:39:37 +0600

    Updated realese card component

--- src/UserAdminDashboard/ReleasesPage/ReleaseCardComponent/ReleaseCardComponent.jsx
+++ src/UserAdminDashboard/ReleasesPage/ReleaseCardComponent/ReleaseCardComponent.jsx
@@ -19,50 +19,51 @@ const ReleaseCardComponent = ({releaseData, totalItems, fetchLoading, currentPag
                 {
                     // eslint-disable-next-line react/prop-types
                     releaseData && releaseData.map(d => 
-                        <div key={d._id} style={{cursor: 'pointer'}} onClick={() => handleNavigate(d._id)}  className="card_parent_div">
-                            <img style={{minHeight: '200px'}} src={d.imgUrl} alt={d.releaseTitle} />
-                            <div className="card_child_div">
-                                <div className="card_content">
-                                    <p className="font-bold text-white">{d.releaseTitle}</p>
-                                    <h3 className="text-xs font-semibold text-slate-300">ID: {d._id}</h3>
+                        <div key={d._id} style={{cursor: 'pointer', borderRadius: '24px', height: '276px', width: '265px'}} onClick={() => handleNavigate(d._id)}  className="p-1 border">
+                            <img className="" style={{width: '100%', height: '191px', borderRadius: '20px'}} src={d.imgUrl} alt={d.releaseTitle} />
+                            <div className="">
+                                <div className="p-3">
+                                    <p className="font-bold text-[#252525] pb-1">{d.releaseTitle}</p>
+                                    <div className="flex justify-between items-center">
+                                        <h3 className="text-[12px] text-[#252525]">UPC {d.UPC}</h3>
+                                        {
+                                            d.status === 'Pending' &&
+                                                <div className="flex items-center">
+                                                    <ClockIcon className="h-3 w-3 text-[#FEB951] me-1"/>
+                                                    <p className="text-xs font-semibold text-[#FEB951]">{d.status}</p>
+                                                </div>
+                                        }
+                                        {
+                                            d.status === 'Approved' &&
+                                                <div className="flex items-center">
+                                                    <CheckBadgeIcon className="h-3 w-3 text-[#39C616] me-1"/>
+                                                    <p className="text-xs font-semibold text-[#39C616]">{d.status}</p>
+                                                </div>
+                                        }
+                                        {
+                                            d.status === 'Action Required' &&
+                                                <div className="flex items-center">
+                                                    <ExclamationTriangleIcon className="h-3 w-3 text-[#71717A] me-1"/>
+                                                    <p className="text-xs font-semibold text-[#71717A]">{d.status}</p>
+                                                </div>
+                                        }
+                                        {
+                                            d.status === 'Takedown' &&
+                                                <div className="flex items-center">
+                                                    <ExclamationTriangleIcon className="h-3 w-3 text-[#FF7050] me-1"/>
+                                                    <p className="text-xs font-semibold text-[#FF7050]">{d.status}</p>
+                                                </div>
+                                        }
+                                        {
+                                            d.status === 'Review' &&
+                                                <div className="flex items-center">
+                                                    <DocumentMagnifyingGlassIcon className="h-3 w-3 text-[#39B5FB] me-1"/>
+                                                    <p className="text-xs font-semibold text-[#39B5FB]">{d.status}</p>
+                                                </div>
+                                        }
+                                    </div>
                                 </div>
                             </div>
-                                {/* Status  */}
-                                {
-                                    d.status === 'Pending' &&
-                                        <div className="flex items-center p-1 music_status bg-[#ffae00] rounded-md shadow">
-                                            <ClockIcon className="h-3 w-3 text-white me-1"/>
-                                            <p className="text-xs font-semibold text-white">{d.status}</p>
-                                        </div>
-                                }
-                                {
-                                    d.status === 'Approved' &&
-                                        <div className="flex items-center p-1 music_status bg-[#00c90d] rounded-md shadow">
-                                            <CheckBadgeIcon className="h-3 w-3 text-white me-1"/>
-                                            <p className="text-xs font-semibold text-white">{d.status}</p>
-                                        </div>
-                                }
-                                {
-                                    d.status === 'Action Required' &&
-                                        <div className="flex items-center p-1 music_status bg-red-700 rounded-md shadow">
-                                            <ExclamationTriangleIcon className="h-3 w-3 text-white me-1"/>
-                                            <p className="text-xs font-semibold text-white">{d.status}</p>
-                                        </div>
-                                }
-                                {
-                                    d.status === 'Takedown' &&
-                                        <div className="flex items-center p-1 music_status bg-red-500 rounded-md shadow">
-                                            <ExclamationTriangleIcon className="h-3 w-3 text-white me-1"/>
-                                            <p className="text-xs font-semibold text-white">{d.status}</p>
-                                        </div>
-                                }
-                                {
-                                    d.status === 'Review' &&
-                                        <div className="flex items-center p-1 music_status bg-info rounded-md shadow">
-                                            <DocumentMagnifyingGlassIcon className="h-3 w-3 text-white me-1"/>
-                                            <p className="text-xs font-semibold text-white">{d.status}</p>
-                                        </div>
-                                }
                         </div>
                     )
                 }
