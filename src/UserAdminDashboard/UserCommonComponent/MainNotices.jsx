bf49ac0fa2665ee073180cb07ab69b35f7ab87dc
mehedy201 <mehedy643@gmail.com>
2024-10-06 11:27:15 +0600

    updeted notce page (desktop version)

--- src/UserAdminDashboard/UserCommonComponent/MainNotices.jsx
+++ src/UserAdminDashboard/UserCommonComponent/MainNotices.jsx
@@ -9,30 +9,35 @@ const MainNotices = () => {
 
     const [noticeData, setNoticeData] = useState();
     useEffect(() => {
-        axios.get(`https://shark-app-65c5t.ondigitalocean.app/admin/api/v1/notice/661089403281a4347e1d3498`)
+        axios.get(`http://localhost:5000/admin/api/v1/notice`)
         .then(res => {
             if(res.status === 200){
                 setNoticeData(res.data.data)
+                console.log(res.data.data);
             }
         })
     },[])
 
 
     return (
-        <div className='flex bg-[#F2F2F2] rounded-md'>            
-
-                    <BellAlertIcon className='w-12 h-12 ps-1 '/>
-                    <div className=''>
-                        <div style={{cursor: 'pointer'}} onClick={() => navigate(`/notice/661089403281a4347e1d3498`)} className="p-2">
-                            <p className=" font-semibold text-[#252525]">{noticeData?.noticeTitle}</p>
-                            <p className="text-[#71717A]">{noticeData?.noticeDescription.slice(0, 50)}...</p>
-                            <div className='flex justify-between items-center pt-2'>
-                                <p className="text-sm text-[#71717A]">{noticeData?.date}</p>
-                                <p className="text-sm text-[#71717A]">{noticeData?.time}</p>
+        <div>
+            {
+                noticeData && noticeData.map(data => 
+                    <div key={data._id} className='flex bg-[#F2F2F2] rounded-md'>            
+                        <BellAlertIcon className='w-12 h-12 ps-1 '/>
+                        <div className=''>
+                            <div style={{cursor: 'pointer'}} onClick={() => navigate(`/notice/661089403281a4347e1d3498`)} className="p-2">
+                                <p className=" font-semibold text-[#252525]">{data?.noticeTitle}</p>
+                                <p className="text-[#71717A]">{data?.noticeDescription.slice(0, 50)}...</p>
+                                <div className='flex justify-between items-center pt-2'>
+                                    <p className="text-sm text-[#71717A]">{data?.date}</p>
+                                    <p className="text-sm text-[#71717A]">{data?.time}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
-
+                )
+            }
         </div>
     );
 };
